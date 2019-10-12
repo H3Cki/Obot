87b3e7636a073575a8061e37cde538cec48b4a8e
H3Cki <H3Ckimail@users.noreply.github.com>
2019-10-12 21:04:56 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,9 +2,12 @@ class Utils:
     @staticmethod
     def thousandSeparate(value,separator='.'):
         v = str(value)
+        if len(v) == 4:
+            return v
+        
         rv = list(reversed(v))
         for i,char in enumerate(rv):
-            if not i%3 and i > 0 and i+1 != len(rv):
+            if not i%3 and i > 0:
                 rv[i] = char+separator
         v = ''.join(list(reversed(rv)))
         return v
