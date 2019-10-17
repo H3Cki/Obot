858d8e641f4b66633b389804696878c1aad28e54
H3Cki <H3Ckimail@users.noreply.github.com>
2019-10-17 02:21:20 +0200

    Update galaxy.py

--- galaxy.py
+++ galaxy.py
@@ -1,4 +1,4 @@
-from buildable_tab import Tab
+from tab import Tab
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -14,10 +14,6 @@ class GalaxyTab(Tab):
     def __init__(self,code):
         self.code = code
         self.url = Tab.base_url + code
-        
-    def open(self):
-        bot.browser.get(self.url)
-        element = WebDriverWait(bot.browser, 10).until(EC.presence_of_element_located((By.ID, "info")))
 
 
 class Galaxy:
